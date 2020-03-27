79b6bc2afb8d54700295c37a70334d37cd696533
Rafael <rafarafaelespinaldiazrafa@gmail.com>
2020-03-27 04:12:00 -0300

    Arreglo de la vista alta medica y realizacion de busquedas por id con ajax

--- ProyectoBFinal/Controllers/AltasController.cs
+++ ProyectoBFinal/Controllers/AltasController.cs
@@ -129,12 +129,43 @@ namespace ProyectoBFinal.Controllers
             base.Dispose(disposing);
         }
         [HttpPost]
-        public JsonResult Duplicador(int clavePaciente)
+        public JsonResult BuscadorNom(int clavePaciente)
         {
             
-            var duplicado = (from p in db.Pacientes
-                             where p.Id == clavePaciente
+            var duplicado = (from i in db.Ingresos
+                             join p in db.Pacientes
+                             on i.Id_Paciente equals p.Id
+                             where i.Id == clavePaciente
                             select p.Nombre).ToList();
+            return Json(duplicado);
+        }
+
+        public JsonResult BuscadorMonto(int clavePaciente)
+        {
+
+            var duplicado = (from i in db.Ingresos
+                             join h in db.Habitaciones
+                             on i.Id_Habitacion equals h.Id
+                             where i.Id == clavePaciente
+                             select h.Precio_dia).ToList();
+            return Json(duplicado);
+        }
+        public JsonResult BuscadorFIn(int clavePaciente)
+        {
+
+            var duplicado = (from i in db.Ingresos
+                             where i.Id == clavePaciente
+                             select i.Fecha_ingreso).ToList();
+            return Json(duplicado);
+        }
+        public JsonResult BuscadorNHab(int clavePaciente)
+        {
+
+            var duplicado = (from i in db.Ingresos
+                             join h in db.Habitaciones
+                             on i.Id_Habitacion equals h.Id
+                             where i.Id == clavePaciente
+                             select h.Numero).ToList();
             return Json(duplicado);
         }
     }
